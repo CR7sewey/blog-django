dd4d12dc96475800db9128005ec8ec107b054338
CR7sewey <miguelmotacarvalho@gmail.com>
2024-01-12 16:31:20 +0000

    Creating new app site_setup, model MenuLink and respective admin

---
+++ djangoapp/site_setup/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from site_setup import models
+# Register your models here.
+
+
+@admin.register(models.MenuLink)
+class MenuLinkAdmin(admin.ModelAdmin):
+    list_display = 'id', 'text', 'url_or_path'
+    list_display_links = 'id', 'text', 'url_or_path',
+    search_fields = 'id', 'text', 'url_or_path',
