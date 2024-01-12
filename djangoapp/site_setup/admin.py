2e9179337c081ab2c82b959b048f685aa2284d51
CR7sewey <miguelmotacarvalho@gmail.com>
2024-01-12 20:59:01 +0000

    Creating SiteSetup model and and admin area

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -8,3 +8,12 @@ class MenuLinkAdmin(admin.ModelAdmin):
     list_display = 'id', 'text', 'url_or_path'
     list_display_links = 'id', 'text', 'url_or_path',
     search_fields = 'id', 'text', 'url_or_path',
+
+
+@admin.register(models.SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description'
+
+    # True -> permission to add new SiteSetup (in admin area)
+    def has_add_permission(self, request):
+        return not models.SiteSetup.objects.exists()
