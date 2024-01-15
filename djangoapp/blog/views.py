3028eeaa8c76448ded6dbc073e9f175fd0597bbd
CR7sewey <miguelmotacarvalho@gmail.com>
2024-01-15 17:04:05 +0000

    Layout

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # Create your views here.
+posts = list(range(1000))
 
 
 def index(request):
-    return render(request, 'blog/pages/index.html',)
+
+    # Put x number os contacts into each page
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {"page_obj": page_obj}
+    return render(request, 'blog/pages/index.html', context,)
 
 
 def post(request):
-    return render(request, 'blog/pages/post.html',)
+    # Put x number os contacts into each page
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {"page_obj": page_obj}
+    return render(request, 'blog/pages/post.html', context,)
 
 
 def page(request):
-    return render(request, 'blog/pages/page.html',)
+    # Put x number os contacts into each page
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {"page_obj": page_obj}
+    return render(request, 'blog/pages/page.html', context,)
