00ac5724c6af403ddbfa2975e46ba374aea2c8b5
CR7sewey <miguelmotacarvalho@gmail.com>
2024-01-16 16:01:47 +0000

    Ressizing images in Post

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from utils.rands import slugify_new
 from django.contrib.auth.models import User
+from utils.images import resize_image
 # Create your models here.
 
 
@@ -131,7 +132,18 @@ class Post(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.title)
 
-        return super().save(*args, **kwargs)
+        current_cover_name = str(self.cover.name)
+        # senao tiver ele nao guard pq é sobreescrito o metodo
+        super_save = super().save(*args, **kwargs)
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover.name
+
+        if cover_changed:
+            # true - optimize, 70 - percentage
+            resize_image(self.cover, 900, True, 70)
+        return super_save
 
     def __str__(self) -> str:
         return self.title
