fd8ad9536fe6aecd37df73455387c254b72e93df
CR7sewey <miguelmotacarvalho@gmail.com>
2024-01-16 19:17:34 +0000

    Configurating django-summernote as HTML editor in TextField in Models Django

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from utils.rands import slugify_new
 from django.contrib.auth.models import User
 from utils.images import resize_image
+from django_summernote.models import AbstractAttachment
 # Create your models here.
 
 
@@ -147,3 +148,21 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class PostAttachment(AbstractAttachment):  # override da classe
+    def save(self, *args, **kwargs):
+        if not self.name:
+            self.name = self.file.name
+
+        current_file_name = str(self.file.name)
+        super_save = super().save(*args, **kwargs)
+        file_changed = False
+
+        if self.file:
+            file_changed = current_file_name != self.file.name
+
+        if file_changed:
+            resize_image(self.file, 900, True, 70)
+
+        return super_save
--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -21,12 +21,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('blog.urls'))
+    path("", include('blog.urls')),
+    path("summernote/", include('django_summernote.urls')),
 ]
 
 if settings.DEBUG:
     # enviar artigos dentro do servidor
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-   # urlpatterns += static(settings.STATIC_URL,
+    # urlpatterns += static(settings.STATIC_URL,
     #                      document_root=settings.STATIC_ROOT)
